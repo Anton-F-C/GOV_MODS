925aa8c530195672d76fff68c1f9b8aa2067f843
Anton <council.anton@gmail.com>
2025-04-03 01:57:09 -0700

    update1.1

--- PromotionPathBuilder.cs
+++ PromotionPathBuilder.cs
@@ -8,17 +8,30 @@ namespace Government
         {
             var db = GovernmentDatabase.AllPositions;
 
+            if (db.ContainsKey(GovernmentRole.Alderman))
+                db[GovernmentRole.Alderman].PromotionPath.Add(GovernmentRole.Mayor);
+
+            if (db.ContainsKey(GovernmentRole.Mayor))
+                db[GovernmentRole.Mayor].PromotionPath.Add(GovernmentRole.CongressOfficial); // Government Official
+
+            if (db.ContainsKey(GovernmentRole.CongressOfficial)) // Government Official
+                db[GovernmentRole.CongressOfficial].PromotionPath.Add(GovernmentRole.Politician); // Diplomat
+
+            if (db.ContainsKey(GovernmentRole.Politician)) // Diplomat
+                db[GovernmentRole.Politician].PromotionPath.Add(GovernmentRole.Senator);
+
+            if (db.ContainsKey(GovernmentRole.Senator))
+                db[GovernmentRole.Senator].PromotionPath.Add(GovernmentRole.VicePresident);
+
+            if (db.ContainsKey(GovernmentRole.VicePresident))
+                db[GovernmentRole.VicePresident].PromotionPath.Add(GovernmentRole.President);
+
+            // Keep prior non-political paths
             if (db.ContainsKey(GovernmentRole.CivilClerk))
                 db[GovernmentRole.CivilClerk].PromotionPath.Add(GovernmentRole.Judge);
 
             if (db.ContainsKey(GovernmentRole.Judge))
                 db[GovernmentRole.Judge].PromotionPath.Add(GovernmentRole.Mayor);
-
-            if (db.ContainsKey(GovernmentRole.Mayor))
-                db[GovernmentRole.Mayor].PromotionPath.Add(GovernmentRole.Senator);
-
-            if (db.ContainsKey(GovernmentRole.Senator))
-                db[GovernmentRole.Senator].PromotionPath.Add(GovernmentRole.President);
 
             if (db.ContainsKey(GovernmentRole.PoliceOfficer))
                 db[GovernmentRole.PoliceOfficer].PromotionPath.Add(GovernmentRole.PoliceChief);
